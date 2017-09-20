2fa9b3750176694aa5106b1045d5105e575fbb1e
tirthjain <jaintirth24@gmail.com>
2017-09-20 19:56:14 +0530

    Vector model changed.

--- vectors.py
+++ vectors.py
@@ -1,4 +1,9 @@
-from math import sqrt, atan, sin, cos
+from sympy import *
+from mpmath import *
+
+# Fro pretty printing
+mp.dps = 15
+mp.pretty = True
 
 
 class Vector:
@@ -9,7 +14,6 @@ class Vector:
     def __init__(self, **kwargs):
         x, y, magnitude, angle = None, None, None, None
         if 'x' in kwargs and 'y' in kwargs:
-            print(kwargs)
             x = kwargs['x']
             y = kwargs['y']
             magnitude = sqrt(x ** 2 + y ** 2)
@@ -21,7 +25,16 @@ class Vector:
         attrs = {'x': x, 'y': y, 'magnitude': magnitude, 'angle': angle}
         self.__dict__.update(attrs)
 
+    def multiply_with_scalar(self, scalar):
+        self.x *= scalar
+        self.y *= scalar
+        self.magnitude *= scalar
 
+    def parallel_unit_vector(self):
+        return Vector(x = cos(self.angle), y= sin(self.angle))
+
+
+# Vector operations.
 def vector_add(*args):
     vx = 0
     vy = 0
@@ -29,4 +42,9 @@ def vector_add(*args):
         vx += v.x
         vy += v.y
     return Vector(x=vx, y=vy)
+
+
+def dot_product(vector1, vector2):
+    return vector1.x * vector2.x + vector1.y + vector2.y
+
 
