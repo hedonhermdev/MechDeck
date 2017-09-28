cb6dd89780c8a29624ff14191b4559d98a4d8e10
tirthjain <jaintirth24@gmail.com>
2017-09-28 19:23:38 +0530

    Fixed example.py

--- example.py
+++ example.py
@@ -8,13 +8,13 @@ matplotlib.rcParams['backend'] = "Qt4Agg"
 vel = v.Vector(i=10, j=0)
 body1 = k.Body('body1', k.origin, vel, v.Vector(i=100, j=0))
 
-time = np.linspace(1, 10, 1000)
+time = np.linspace(1, 10, 100)
 
 pos = []
 
 
 def calc_position(t):
-    return body1.velocity.i * t + 0.5 * body1.acceleration.i * t**0.5
+    return body1.velocity.i * t + 0.5 * body1.acceleration.i * t**2
 
 for t in time:
     position = calc_position(t)
@@ -22,8 +22,8 @@ for t in time:
 print(time)
 print(pos)
 
-plt.plot(time, pos, 'ro')
+plt.plot(time, pos, 'r--')
 plt.xlabel('Time')
 plt.ylabel('Position')
 plt.savefig('foo.png')
-plt.show()
+#plt.show()
